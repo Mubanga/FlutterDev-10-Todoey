25769a3752bb6b395e6e199339eb213cd8dc36f4
Mubanga <mubangeriseme@yahoo.com>
2019-08-25 00:52:11 +0200

    Todoey FloatingActionButton Added

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -11,6 +11,10 @@ class _TaskScreenState extends State<TaskScreen> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.lightBlueAccent,
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.lightBlueAccent,
+            child: Icon(Icons.add),
+            onPressed: () {}),
         body: Column(
           children: <Widget>[
             /**
@@ -19,36 +23,39 @@ class _TaskScreenState extends State<TaskScreen> {
             Padding(
               padding: EdgeInsets.only(
                   top: 50.0, left: 30.0, right: 30.0, bottom: 30.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundColor: Colors.white,
-                    radius: 30.0,
-                    child: Icon(
-                      Icons.list,
-                      color: Colors.lightBlueAccent,
-                      size: 30.0,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 30.0,
+                      child: Icon(
+                        Icons.list,
+                        color: Colors.lightBlueAccent,
+                        size: 30.0,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text(
-                    "Todoey",
-                    style: TextStyle(
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "Todoey",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    Text(
+                      "12 Tasks",
+                      style: TextStyle(
                         color: Colors.white,
-                        fontSize: 50.0,
-                        fontWeight: FontWeight.w700),
-                  ),
-                  Text(
-                    "12 Tasks",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                    ),
-                  )
-                ],
+                        fontSize: 18,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
             /**
